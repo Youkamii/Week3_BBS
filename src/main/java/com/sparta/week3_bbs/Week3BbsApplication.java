931025267816452aba3ec6f84e9e48538b485326
Youkamii <gkfkd747@naver.com>
2023-11-06 21:11:25 +0900

    Create : 프로젝트 생성

---
+++ src/main/java/com/sparta/week3_bbs/Week3BbsApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.week3_bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3BbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3BbsApplication.class, args);
+	}
+
+}
